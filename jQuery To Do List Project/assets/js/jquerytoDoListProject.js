81a4b77c180bb31f7fa31800bf1380a67dc200ac
Gerard <gmoneymaster24@yahoo.com>
2017-06-13 15:06:13 -0400

    To Do project using jQuery
    Add things on To Do list. Delete things off of To Do list.

---
+++ jQuery To Do List Project/assets/js/jquerytoDoListProject.js
@@ -0,0 +1,44 @@
+//check off specific todo by clicking
+// $('li').click(function(){
+//     if($(this).css('color') === 'rgb(128, 128, 128)') {
+//        $(this).css({
+//            color: 'black',
+//            textDecoration: 'none'
+//        });
+//     } else{$(this).css({
+//        color: 'gray',
+//        textDecoration: "line-through"
+//         });
+//     }
+// });
+
+// $('li').click(function(){
+//     $(this).toggleClass('completed');
+// });
+
+$('ul').on('click', 'li', function(){
+    $(this).toggleClass('completed');
+});
+
+//click on X to delete todo
+$('ul').on('click', 'span', function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //grabbing text from input
+       var todoText =  $(this).val();
+       //clear input 
+       $(this).val('');
+       //create a new li and add to ul
+       $('ul').append('<li><span><i class="fa fa-trash" aria-hidden="true"></i></span> ' + todoText +'</li>')
+    }
+});
+
+$('.fa-plus').click(function(){
+    $("input[type='text']").fadeToggle();
+});
